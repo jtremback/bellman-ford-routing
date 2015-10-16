a448cb1600169f4e818d90e827bea8d554b34fd5
Jehan Tremback <jehan@Jehans-MacBook-Pro.local>
2015-10-15 17:46:44 -0700

    cleanup package.json, correct some logic, rename variables for clarity

--- index.js
+++ index.js
@@ -45,33 +45,34 @@ let infinityJSON = {
 //     1\|
 //       C
 
-// let network = {
-//   nodes: {
-//     S: {},
-//     A: {},
-//     B: {},
-//     C: {}
-//   },
-//   edges: {
-//     'S->A': { cost: 1 },
-//     'A->S': { cost: 1 },
+let network = {
+  nodes: {
+    S: {},
+    A: {},
+    B: {},
+    C: {}
+  },
+  edges: {
+    'S->A': { cost: 1 },
+    'A->S': { cost: 1 },
 
-//     'B->A': { cost: 1 },
-//     'A->B': { cost: 1 },
+    'B->A': { cost: 1 },
+    'A->B': { cost: 1 },
 
-//     'B->C': { cost: 1 },
-//     'C->B': { cost: 1 },
+    'B->C': { cost: 1 },
+    'C->B': { cost: 1 },
 
-//     'C->A': { cost: 1 },
-//     'A->C': { cost: 1 },
-//   }
-// }
+    'C->A': { cost: 1 },
+    'A->C': { cost: 1 },
+  }
+}
 
-// setTimeout(() => {
-//   network.edges['S->A'].cost = Infinity
-//   network.edges['A->S'].cost = Infinity
-//   ui.updateNetwork(network)
-// }, 4 * tm)
+setTimeout(() => {
+  debugger
+  network.edges['S->A'].cost = Infinity
+  network.edges['A->S'].cost = Infinity
+  ui.updateNetwork(network)
+}, 4 * tm)
 
 
 // Can we select the better route?
@@ -137,30 +138,30 @@ let infinityJSON = {
 //  \  |
 // 1 --B
 
-let network = {
-  nodes: {
-    S: {},
-    A: {},
-    B: {}
-  },
-  edges: {
-    'S->A': { cost: 1 },
-    'A->S': { cost: 1 },
+// let network = {
+//   nodes: {
+//     S: {},
+//     A: {},
+//     B: {}
+//   },
+//   edges: {
+//     'S->A': { cost: 1 },
+//     'A->S': { cost: 1 },
 
-    'B->A': { cost: 1 },
-    'A->B': { cost: 1 },
+//     'B->A': { cost: 1 },
+//     'A->B': { cost: 1 },
 
-    'S->B': { cost: 2 },
-    'B->S': { cost: 2 },
-  }
-}
+//     'S->B': { cost: 2 },
+//     'B->S': { cost: 2 },
+//   }
+// }
 
-setTimeout(() => {
-  debugger
-  network.edges['S->A'].cost = Infinity
-  network.edges['A->S'].cost = Infinity
-  ui.updateNetwork(network)
-}, 4 * tm)
+// setTimeout(() => {
+//   debugger
+//   network.edges['S->A'].cost = Infinity
+//   network.edges['A->S'].cost = Infinity
+//   ui.updateNetwork(network)
+// }, 4 * tm)
 
 
 
@@ -186,21 +187,27 @@ function initNodes (network) {
   }
 }
 
-function feasibilityCondition (self, newSource, existingSource) {
+function feasibilityCondition (self, from, update, source) {
+  // if (update.cost === Infinity) {debugger}
   // Don't accept updates for yourself
-  if (newSource.id === self.id) {
+  if (update.id === self.id) {
     return false
 
   // If a route for this destination does not yet exist, accept it.
-  } else if (!existingSource) {
-    console.log('!existingSource')
+  } else if (!source) {
+    console.log('!source')
+    return true
+
+  // If it is an adjustment to the currently selected route
+  } else if (from.id === source.nextHop) {
+    console.log('from.id === source.id')
     return true
 
   // If the edge to the neighbor is lower cost than the smallest metric that
   // we have ever advertised to our neighbors
   // (D(B) < FD(A) from section 2.4)
-  } else if (newSource.cost < existingSource.fd) {
-    console.log('newSource.cost < existingSource.fd')
+  } else if (update.cost < source.fd) {
+    console.log('update.cost < source.fd')
     return true
 
   } else {
@@ -208,36 +215,34 @@ function feasibilityCondition (self, newSource, existingSource) {
   }
 }
 
-function receiveUpdate (self, from, newSources) {
-  newSources = infinityJSON.parse(newSources)
+function receiveUpdate (self, from, updates) {
+  updates = infinityJSON.parse(updates)
 
   // Get cost of edge from network spec
   // (Simulates hello and IHU from section 3.4.3)
   let edge = network.edges[self.id + '->' + from.id]
   let edgeCost = (edge && edge.cost) || 0
 
-  for (let newSource of newSources) {
-    let existingSource = self.sources[newSource.id]
-
+  for (let update of updates) {
     // If feasibility condition is true, accept update
-    if (feasibilityCondition(self, newSource, existingSource)) {
+    if (feasibilityCondition(self, from, update, self.sources[update.id])) {
 
       // If the source does not exist, make a new one
-      if (!self.sources[newSource.id]) {
-        self.sources[newSource.id] = { cost: newSource.cost + edgeCost, nextHop: from.id, fd: Infinity }
+      if (!self.sources[update.id]) {
+        self.sources[update.id] = { cost: update.cost + edgeCost, nextHop: from.id, fd: Infinity }
 
       // Otherwise, modify in place
       } else {
-        self.sources[newSource.id].cost = newSource.cost + edgeCost
-        self.sources[newSource.id].nextHop = from.id
+        self.sources[update.id].cost = update.cost + edgeCost
+        self.sources[update.id].nextHop = from.id
       }
 
-      ui.log(`${self.id} accepted update ${infinityJSON.stringify(newSource)} from ${from.id}, with feasibility distance ${existingSource.fd}`)
+      ui.log(`${self.id} accepted update ${infinityJSON.stringify(update)} from ${from.id} to source ${JSON.stringify(self.sources[update.id])}`)
       ui.updateNetwork(network)
 
     // Reject update
     } else {
-      ui.log(`${self.id} rejected update ${infinityJSON.stringify(newSource)} from ${from.id}`)
+      ui.log(`${self.id} rejected update ${infinityJSON.stringify(update)} from ${from.id}`)
     }
   }
 }
@@ -254,7 +259,7 @@ function transmit (neighbor, self, sources) {
 }
 
 function sendPeriodicUpdate (self) {
-  let sources = []
+  let updates = []
 
   for (let sourceId in self.sources) {
     let source = self.sources[sourceId]
@@ -266,13 +271,13 @@ function sendPeriodicUpdate (self) {
     }
 
     // Add source to message going out
-    sources.push({ id: sourceId, cost: source.cost })
+    updates.push({ id: sourceId, cost: source.cost })
   }
 
   for (let neighborId in self.neighbors) {
     let neighbor = self.neighbors[neighborId]
     try {
-      transmit(neighbor, self, sources)
+      transmit(neighbor, self, updates)
     } catch (e) {
       // If the cost is infinity, update the source table
       self.sources[neighborId].cost = Infinity
